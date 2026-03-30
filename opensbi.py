0370222aab5a2158672d4541071f7c8d282bda0d
Michael Cobb <michael.cobb@thecapablehub.org>
2026-03-30 17:41:39 +0100

    opensbi: Initial commit working example
    Signed-Off-By: Michael Cobb (The Capable Hub) <mcobb@thegoodpenguin.co.uk>

---
+++ opensbi.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from main import *
+
+def get_config():
+    return {
+        "source": {
+            "git": {
+                "remote": "https://github.com/CHERI-Alliance/opensbi.git",
+                "branch": "codasip-cheri-riscv"
+            }
+        },
+        "matcher": {
+            "fn": match_file,
+            "args": [
+                "include/sbi/sbi_version.h",
+                [
+                    "^#define OPENSBI_VERSION_MAJOR (?P<major>\\d+)",
+                    "^#define OPENSBI_VERSION_MINOR (?P<minor>\\d+)"
+                ]
+            ]
+        }
+    }
+
+if __name__ == "__main__":
+    extract_version(get_config())
