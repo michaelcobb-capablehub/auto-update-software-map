8919ccfaeca3d30174c9aa12aa18a930c6a666c0
Michael Cobb <michael.cobb@thecapablehub.org>
2026-04-23 11:45:34 +0100

    SemanticVersion: Add support for "extra" string at end of version, switch to regex-based parsing
    Signed-Off-By: Michael Cobb (The Capable Hub) <mcobb@thegoodpenguin.co.uk>

--- version.py
+++ version.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
+import re
 
 class Version():
     def __init__(self):
         pass
 
 class SemanticVersion(Version):
-    def __init__(self, major=None, minor=None, patch=None):
+    def __init__(self, major=None, minor=None, patch=None, extra=None):
         Version.__init__(self)
         self._major = int(major) if major else None
         self._minor = int(minor) if minor else None
         self._patch = int(patch) if patch else None
+        self._extra = str(extra) if extra else None
 
     @staticmethod
     def parse(s):
-        parts = s.split(".")
-        if len(parts) < 2 or len(parts) > 3:
-            raise RuntimeError(f"'{s}': Unable to parse semantic version string")
-        return SemanticVersion(*parts)
+        pattern = r"^(?P<major>\d+)\.(?P<minor>\d+)(\.(?P<patch>\d+))?(-(?P<extra>[\w\-]+))?$"
+        m = re.match(pattern, s)
+        if m is None:
+            raise RuntimeError(f"'{s}': Could not parse")
+        major = m.group("major")
+        minor = m.group("minor")
+        patch = m.group("patch")
+        extra = m.group("extra")
+        return SemanticVersion(major, minor, patch, extra)
 
     def __str__(self):
         s = ""
@@ -26,9 +33,11 @@ class SemanticVersion(Version):
             s += "." + str(self._minor)
         if self._patch is not None:
             s += "." + str(self._patch)
+        if self._extra is not None:
+            s += "-" + str(self._extra)
         return s
 
 def make_version(version_type, version_info):
     if version_type == "semantic":
-        return SemanticVersion(version_info.get("major"), version_info.get("minor"), version_info.get("patch"))
+        return SemanticVersion(version_info.get("major"), version_info.get("minor"), version_info.get("patch"), version_info.get("extra"))
     raise RuntimeError(f"Unhandled version_type: {version_type}")
